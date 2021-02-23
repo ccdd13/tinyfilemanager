9dcd621f6f2a7598638da8f47297f5e21eff47e3
ccdd13 <ccdd13@users.noreply.github.com>
2021-02-23 17:26:21 +0100

    update admin username/password
    Signed-off-by: ccdd13 <ccdd13@users.noreply.github.com>

--- config.php
+++ config.php
@@ -20,7 +20,7 @@ $use_auth = true;
 // Users: array('Username' => 'Password', 'Username2' => 'Password2', ...)
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
-    'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
+    'root' => password_hash('adm#passwd..HERE', PASSWORD_DEFAULT),
 );
 
 // Readonly users
